a926c68a91c35fb5888cec63287ce79b379cbde5
Jean-Philippe ETRILLARD <jphetr@gmail.com>
2020-11-01 10:25:50 +0100

    Initial commit

---
+++ precache-manifest.83a305d107de27650a7b9d89cc9b879d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e844b593ad4e87a7b6128b12b455fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44bd97e516a349f840ed",
+    "url": "/static/js/2.ec00071a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ec00071a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c74cf3ca3046507994",
+    "url": "/static/js/main.5940a508.chunk.js"
+  },
+  {
+    "revision": "83b9524c1892f1cc9a88",
+    "url": "/static/js/runtime-main.067f0582.js"
+  }
+]);
